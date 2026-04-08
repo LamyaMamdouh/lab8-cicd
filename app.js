2dadb0bb1ecce24172ade40358c3a355337332c6
Ubuntu <ubuntu@ip-172-31-25-8.ec2.internal>
2026-04-08 11:31:35 +0000

    Fix app.js merge conflict

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello Lab8-CICD!');
-});
-
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-});
-=======
 const mysql = require('mysql2/promise');
 const app = express();
 app.use(express.json());
@@ -30,4 +18,3 @@ app.get('/tasks', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('App running on port 3000'));
->>>>>>> 27eb5a7cc4c987cf127eda4c810e47bab0a2214c
